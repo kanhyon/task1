6d6185c1f3174e16cdcf2ade809cce36f03d1e98
李康鉉 <kanhyon@kanhyonMacBook-Air.local>
2020-06-02 05:48:53 +0900

    カード機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details o the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'top#index'
+  resources :list, only: %i(new create)
+  resources :list, only: %i(new create edit update destroy) do
+    resources :card, only: %i(new create show)
+  end
 end
