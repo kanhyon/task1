12d9d231362388cb4dcbd672c6385eb1c743773e
李康鉉 <kanhyon@kanhyonMacBook-Air.local>
2020-06-06 03:46:44 +0900

    リスト編集、削除機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details o the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'top#index'
-  resources :list, only: %i(new create)
   resources :list, only: %i(new create edit update destroy) do
-    resources :card, except: %i(index)
+  resources :card, except: %i(index)
+  end
 end
